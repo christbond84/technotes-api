64075774437b0067c73ce7d3894a4e7bc62dc411
Joseph George <christbond84@gmail.com>
2024-11-09 12:23:01 +0530

    test

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,12 +3,11 @@ const router = express.Router()
 const usersController = require("../controllers/usersController")
 const verifyAdminJWT = require("../middleware/verifyAdminJWT")
 
-router.use(verifyAdminJWT)
 router
   .route("/")
   .get(usersController.getAllUsers)
-  .post(usersController.createNewUser)
-  .patch(usersController.updateUser)
-  .delete(usersController.deleteUser)
+  .post(verifyAdminJWT, usersController.createNewUser)
+  .patch(verifyAdminJWT, usersController.updateUser)
+  .delete(verifyAdminJWT, usersController.deleteUser)
 
 module.exports = router
